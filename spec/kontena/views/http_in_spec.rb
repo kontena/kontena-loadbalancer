006b58a7bb2ebd7403a141abb5cc921db145e130
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-12-08 07:54:16 +0200

    allow to enable accept-proxy via env (#38)

---
+++ spec/kontena/views/http_in_spec.rb
@@ -0,0 +1,60 @@
+describe Kontena::Views::HttpIn do
+
+  describe '.render' do
+    context 'bind' do
+      it 'bings to port 80' do
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/bind \*:80/)).to be_truthy
+      end
+
+      it 'does not accept proxy protocol by default' do
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/accept-proxy/)).to be_falsey
+      end
+
+      it 'accepts proxy protocol if env is set' do
+        allow(ENV).to receive(:[])
+        allow(ENV).to receive(:[]).with('KONTENA_LB_ACCEPT_PROXY').and_return('true')
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/accept-proxy/)).to be_truthy
+      end
+
+      it 'does not bind to port 443 by default' do
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/bind \*:443/)).to be_falsey
+      end
+
+      it 'binds to port 443 if SSL certs exist' do
+        allow(ENV).to receive(:[])
+        allow(ENV).to receive(:[]).with('SSL_CERTS').and_return('certs')
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/bind \*:443/)).to be_truthy
+      end
+
+      it 'supports http2 if SSL certs exist' do
+        allow(ENV).to receive(:[])
+        allow(ENV).to receive(:[]).with('SSL_CERTS').and_return('certs')
+        output = described_class.render(
+          format: :text,
+          services: []
+        )
+        expect(output.match(/alpn h2/)).to be_truthy
+      end
+    end
+  end
+end
--- spec/kontena/views/tcp_proxies_spec.rb
+++ spec/kontena/views/tcp_proxies_spec.rb
@@ -4,6 +4,37 @@ describe Kontena::Views::TcpProxies do
   let(:upstream_class) { Kontena::Models::Upstream }
 
   describe '.render' do
+    context 'bind' do
+      it 'sets accept-proxy if env is set' do
+        allow(ENV).to receive(:[]).with('KONTENA_LB_ACCEPT_PROXY').and_return('true')
+        services = [
+          service_class.new('foo').tap { |s|
+            s.external_port = 8080
+            s.upstreams = [upstream_class.new('foo-1', '10.81.3.2:8080')]
+          }
+        ]
+        output = described_class.render(
+          format: :text,
+          services: services
+        )
+        expect(output.match(/accept-proxy/)).to be_truthy
+      end
+
+      it 'does not set accept-proxy without env' do
+        services = [
+          service_class.new('foo').tap { |s|
+            s.external_port = 8080
+            s.upstreams = [upstream_class.new('foo-1', '10.81.3.2:8080')]
+          }
+        ]
+        output = described_class.render(
+          format: :text,
+          services: services
+        )
+        expect(output.match(/accept-proxy/)).to be_falsey
+      end
+    end
+
     context 'balance' do
       it 'sets balance to leastconn by default' do
         services = [
